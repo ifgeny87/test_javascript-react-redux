1e082aa8694bdb70797fc222975b4610fef4a74b
ifgeny87 <ifgeny87@gmail.com>
2016-11-04 18:03:46 +0500

    # Вторая версия.
    Подключен React и babel. Теперь можно писать полноценные приложения на React.js.
    
    При измении кода в браузере происходит автоматическое обновление за счет HRM.

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.get('/', function (req, res) {
 app.listen(port, function (error) {
     if (error) {
         console.error(error);
-
     } else {
-        console.info('==>  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+        console.info('==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
     }
 });
